a54a5321d91d7d8d2d34458c1fbee0c1ba5702b1
Alexander Bauer <sasha@linux.com>
2015-03-29 13:36:57 -0400

    Add hackafe module for getting statistics

--- _modules/hackafe.py
+++ _modules/hackafe.py
@@ -6,3 +6,19 @@ def espeak(*words):
         return True
     else:
         return False
+
+def stats():
+    temperaturestrs = __salt__['mine.get']('*', 'sensor.temperature')
+    print(temperaturestrs)
+    temperatures = []
+    for tempstr in temperaturestrs:
+        try:
+            temperatures.append(float(tempstr))
+        except ValueError:
+            print("Could not convert: '%s'" % tempstr)
+
+    if temperatures:
+        avgtemp = sum(temperatures)/len(temperatures)
+    else:
+        avgtemp = None
+    return [avgtemp]

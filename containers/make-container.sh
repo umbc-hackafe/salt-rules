95e4d4f2cc60f2fd74e9b7c98ff2ade8035ec4bc
Mark <mark25@dom.hackafe.net>
2015-11-14 20:41:37 -0500

    Moved container directories to reduce number of symlinks

--- containers/make-container.sh
+++ containers/make-container.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/bash
 [ -z "$1" ] && echo "Need to select container name" && exit 1;
 
-if [ -d "/data/$1" ]; then
+if [ -d "/var/lib/machines/$1" ]; then
   exit 0;
 else
-  mkdir /data/$1
+  mkdir /var/lib/machines/$1
   mkdir /data/overlay/$1
   mkdir /data/work/$1
   
   cp /etc/fstab /etc/fstab.bak
-  echo "/data/$1		/data/$1	overlay	rw,relatime,lowerdir=/data/baseroot,upperdir=/data/overlay/$1,workdir=/data/work/$1	0 0" >> /etc/fstab
+  echo "/var/lib/machines/$1		/var/lib/machines/$1	overlay	rw,relatime,lowerdir=/data/baseroot,upperdir=/data/overlay/$1,workdir=/data/work/$1	0 0" >> /etc/fstab
 
-  mount /data/$1
+  mount /var/lib/machines/$1
 
-  systemd-machine-id-setup --root=/data/$1
-  
-  ln -s /data/$1 /var/lib/machines/$1
+  systemd-machine-id-setup --root=/var/lib/machines/$1
 fi

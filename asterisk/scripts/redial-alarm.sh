1d6ef5c38c1f46b268b22ad8e02ecf40a24e6d3e
Dylan Whichard <dylan@whichard.com>
2015-11-17 01:00:45 -0500

    Fix references to /usr/local/bin

--- asterisk/scripts/redial-alarm.sh
+++ asterisk/scripts/redial-alarm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-( sleep 0; /usr/local/bin/make-call-context "$1" super-alarm start "ARRAY(TARGET,CODE)" "$1\\,$2" ) &
+( sleep 0; /usr/bin/make-call-context "$1" super-alarm start "ARRAY(TARGET,CODE)" "$1\\,$2" ) &

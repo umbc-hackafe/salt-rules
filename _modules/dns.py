e817228164df02d479f5cc617f66b4896429a1d5
Dylan Whichard <dwhichard@koolspan.com>
2016-06-23 22:40:40 -0400

    Add listdict support to dns.merge util

--- _modules/dns.py
+++ _modules/dns.py
@@ -1,19 +1,42 @@
 from copy import deepcopy
 
+def is_listdict(d):
+    return isinstance(d, list) and all((isinstance(n, dict) and len(n) == 1 for n in d))
+
+def merge_listdict(a, b):
+    "merges b into a"
+
+    a_dict = {}
+    b_dict = {}
+
+    for elm in a:
+        a_dict.update(elm)
+
+    for elm in b:
+        b_dict.update(elm)
+
+    res_dict = merge(a_dict, b_dict)
+
+    return [{k: v} for k, v in res_dict.items()]
+
 def merge(a, b, path=None):
     "merges b into a"
     if path is None: path = []
-    for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
-                pass # same leaf value
+
+    if is_listdict(a) and is_listdict(b):
+        return merge_listdict(a, b)
+    else:
+        for key in b:
+            if key in a:
+                if isinstance(a[key], dict) and isinstance(b[key], dict):
+                    merge(a[key], b[key], path + [str(key)])
+                elif a[key] == b[key]:
+                    pass # same leaf value
+                else:
+                    a[key] = b[key]
             else:
                 a[key] = b[key]
-        else:
-            a[key] = b[key]
-    return a
+        return a
 
 def static_resolve(host):
     if host == 'localhost':
